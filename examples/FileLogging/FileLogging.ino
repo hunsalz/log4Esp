eeb3b85a3a0e9eba5cbbb93df731bd0a219e0b5a
hunsalz <info@hunsalz.de>
2017-12-29 00:40:43 +0100

    add a poem from Goethe as example

--- examples/FileLogging/FileLogging.ino
+++ examples/FileLogging/FileLogging.ino
@@ -6,7 +6,7 @@
 using log4arduino::LOG;
 using log4arduino::RollingFileAppender;
 
-const char* FILENAME = "/test.log";
+const char* FILENAME = "/test3.log";
 
 void outputLogFile() {
 
@@ -29,15 +29,36 @@ void setup() {
 
   Serial.begin(115200);
   Serial.setDebugOutput(false);
-  while(!Serial && !Serial.available()) {}
+  while (!Serial && !Serial.available()) {}
   delay(300);
   Serial.println();
 
   SPIFFS.begin(); // don't forget to start SPIFFS first
 
-  LOG.getAppender().push_back(new RollingFileAppender(FILENAME));
+  LOG.getAppender().push_back(new RollingFileAppender(FILENAME, 32, 10, true));
 
-  LOG.verbose("Write this text to [%s]", FILENAME); // now, this message is logged to serial and to file
+  // write the first two verse of a poem from Johann Wolfgang von Goethe to file
+  LOG.verbose("Der Zauberlehrling");
+  LOG.verbose("");
+  LOG.verbose("Hat der alte Hexenmeister");
+  LOG.verbose("Sich doch einmal wegbegeben!");
+  LOG.verbose("Und nun sollen seine Geister");
+  LOG.verbose("Auch nach meinem Willen leben.");
+  LOG.verbose("Seine Wort und Werke");
+  LOG.verbose("Merkt ich und den Brauch,");
+  LOG.verbose("Und mit Geistesstärke");
+  LOG.verbose("Tu ich Wunder auch.");
+
+  LOG.verbose("Walle! walle");
+  LOG.verbose("Manche Strecke,");
+  LOG.verbose("Daß, zum Zwecke,");
+  LOG.verbose("Wasser fließe");
+  LOG.verbose("Und mit reichem, vollem Schwalle");
+  LOG.verbose("Zu dem Bade sich ergieße.");
+
+  LOG.verbose("https://de.wikipedia.org/wiki/Der_Zauberlehrling");
+
+  LOG.verbose("Johann Wolfgang von Goethe");
 
   outputLogFile();
 }
