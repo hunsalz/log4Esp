415e48f5953b398d767c629ac12cc6959af19529
hunsalz <info@hunsalz.de>
2017-12-29 14:03:57 +0100

    put output in separate function

--- examples/FileLogging/FileLogging.ino
+++ examples/FileLogging/FileLogging.ino
@@ -3,16 +3,40 @@
 
 #include <FS.h> // https://github.com/esp8266/Arduino/tree/master/cores/esp8266/FS.h
 
-using log4arduino::LOG;
+using log4arduino::Logger;
 using log4arduino::RollingFileAppender;
 
-const char* FILENAME = "/test3.log";
+const char* FILENAME = "/poem.log";
 
-void outputLogFile() {
+void writeLogFile(Logger log) {
 
-  Serial.println("+----------------+");
-  Serial.println("| Output LogFile |");
-  Serial.println("+----------------+");
+  // write the first two verse of a poem from Johann Wolfgang von Goethe to file
+  log.verbose("Der Zauberlehrling");
+  log.verbose("");
+  log.verbose("Hat der alte Hexenmeister");
+  log.verbose("Sich doch einmal wegbegeben!");
+  log.verbose("Und nun sollen seine Geister");
+  log.verbose("Auch nach meinem Willen leben.");
+  log.verbose("Seine Wort und Werke");
+  log.verbose("Merkt ich und den Brauch,");
+  log.verbose("Und mit Geistesstärke");
+  log.verbose("Tu ich Wunder auch.");
+
+  log.verbose("Walle! walle");
+  log.verbose("Manche Strecke,");
+  log.verbose("Daß, zum Zwecke,");
+  log.verbose("Wasser fließe");
+  log.verbose("Und mit reichem, vollem Schwalle");
+  log.verbose("Zu dem Bade sich ergieße.");
+
+  log.verbose("https://de.wikipedia.org/wiki/Der_Zauberlehrling");
+
+  log.verbose("Johann Wolfgang von Goethe");
+}
+
+void readLogFile() {
+
+  Serial.printf("\nOutput log file: %s\n\n", FILENAME);
 
   if (SPIFFS.exists(FILENAME)) {
     File file = SPIFFS.open(FILENAME, "r");
@@ -35,32 +59,11 @@ void setup() {
 
   SPIFFS.begin(); // don't forget to start SPIFFS first
 
-  LOG.getAppender().push_back(new RollingFileAppender(FILENAME, 32, 10, true));
+  Logger log;
+  log.getAppender().push_back(new RollingFileAppender(FILENAME, 32, 10, true));
 
-  // write the first two verse of a poem from Johann Wolfgang von Goethe to file
-  LOG.verbose("Der Zauberlehrling");
-  LOG.verbose("");
-  LOG.verbose("Hat der alte Hexenmeister");
-  LOG.verbose("Sich doch einmal wegbegeben!");
-  LOG.verbose("Und nun sollen seine Geister");
-  LOG.verbose("Auch nach meinem Willen leben.");
-  LOG.verbose("Seine Wort und Werke");
-  LOG.verbose("Merkt ich und den Brauch,");
-  LOG.verbose("Und mit Geistesstärke");
-  LOG.verbose("Tu ich Wunder auch.");
-
-  LOG.verbose("Walle! walle");
-  LOG.verbose("Manche Strecke,");
-  LOG.verbose("Daß, zum Zwecke,");
-  LOG.verbose("Wasser fließe");
-  LOG.verbose("Und mit reichem, vollem Schwalle");
-  LOG.verbose("Zu dem Bade sich ergieße.");
-
-  LOG.verbose("https://de.wikipedia.org/wiki/Der_Zauberlehrling");
-
-  LOG.verbose("Johann Wolfgang von Goethe");
-
-  outputLogFile();
+  writeLogFile(log);
+  readLogFile();
 }
 
 void loop() {
