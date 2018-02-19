a40c737e594405b164d1196f14fa01ac96c4d27b
hunsalz <info@hunsalz.de>
2018-02-19 09:30:41 +0100

    renamed

---
+++ travis/build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+function build_sketches() {
+    
+    local arduino=$1
+    local srcpath=$2
+    local platform=$3
+    local sketches=$(find $srcpath -name *.ino)
+    for sketch in $sketches; do
+        local sketchdir=$(dirname $sketch)
+        echo -e "\n\n ------------ Building $sketch ------------ \n\n";
+        $arduino --verify $sketch;
+        local result=$?
+        if [ $result -ne 0 ]; then
+            echo "Build failed ($1)"
+            return $result
+        fi
+    done
+}
